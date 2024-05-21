395e5fcb9ea852602bfc68ca71cfbb68b5868d3e
ismailemayej <salikbd.com@gmai.com>
2024-05-21 11:48:58 +0600

    layout something change

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -1,13 +1,16 @@
 import Footer from "@/components/shared/Footer/Footer";
 import Navbar from "@/components/shared/Navbar/Navbar";
+import Container from "@/components/ui/Container";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Navbar />
-      <div className="lg:col-span-9 col-span-9 m-2 h-screen  rounded-xl p-3 border">
-        {children}
-      </div>
+      <Container className=" h-screen">
+        <div className="lg:col-span-9 col-span-9 m-2 rounded-xl p-3 border">
+          {children}
+        </div>
+      </Container>
       <Footer />
     </div>
   );
